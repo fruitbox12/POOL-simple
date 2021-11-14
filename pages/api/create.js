db57ad5869afbade8c664baf77c5ac1662cc8ac7
Russell Creswell <rucreswe@cisco.com>
2021-11-14 01:02:17 -0500

    Fixing

--- pages/api/create.js
+++ pages/api/create.js
@@ -1,11 +1,12 @@
-import { useLogin } from "../../context/UserContext";
+// import { useLogin } from "../../context/UserContext";
 
 export default function handler(req, res) {
     // Get data from your database
-    try {
-        login(req.body.email);
-        res.status(200).json({"message": "Success", "req": req});
-    } catch (error) {
-        res.status(200).json({"message": "fail", "req": req});
-    }
+    res.status(200).json({"message": "Success2"});
+    // try {
+    //     login(req.body.email);
+    //     res.status(200).json({"message": "Success", "req": req});
+    // } catch (error) {
+    //     res.status(200).json({"message": "fail", "req": req});
+    // }
 }
