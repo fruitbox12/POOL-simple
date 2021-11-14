47761219f6084725e390e8330c813158da1aad5a
Russell Creswell <rucreswe@cisco.com>
2021-11-14 01:50:54 -0500

    aaa

--- pages/api/create.js
+++ pages/api/create.js
@@ -6,10 +6,10 @@ export default function handler(req, res) {
         var login = require("../../context/UserContext");
         // const login = useLogin();
         // console.log("two");
-        // login(req.body.email);
+        login(req.body.email);
         // console.log("three");
         res.status(200).json({"message": "Success", "email": req.body.email});
     } else {
-        res.status(200).json({"message": "Please use a POST request4."});
+        res.status(200).json({"message": "Please use a POST request5."});
     }
 }
