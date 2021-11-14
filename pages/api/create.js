39f50a42bc446f231617954eb58cf64e0f0e8a6f
Russell Creswell <rucreswe@cisco.com>
2021-11-14 01:13:04 -0500

    Fixing

--- pages/api/create.js
+++ pages/api/create.js
@@ -2,5 +2,6 @@
 
 export default function handler(req, res) {
     // Get data from your database
+    login(req.body.email);
     res.status(200).json({"message": "Success", "email": req.body.email});
 }
