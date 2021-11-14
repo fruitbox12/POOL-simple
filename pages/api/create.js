219b0e06c009a785d72ed1de6349898e427e0a0b
Russell Creswell <rucreswe@cisco.com>
2021-11-14 01:30:06 -0500

    Fixing again

--- pages/api/create.js
+++ pages/api/create.js
@@ -1,14 +1,14 @@
-// import { useLogin } from "../../context/UserContext";
+import { useLogin } from "../../context/UserContext";
 
 export default function handler(req, res) {
-    // if (req.method === 'POST') {
-    //     console.log("one");
-    //     const login = useLogin();
-    //     console.log("two");
-    //     login(req.body.email);
-    //     console.log("three");
-    //     res.status(200).json({"message": "Success", "email": req.body.email});
-    // } else {
+    if (req.method === 'POST') {
+        console.log("one");
+        const login = useLogin();
+        console.log("two");
+        login(req.body.email);
+        console.log("three");
+        res.status(200).json({"message": "Success", "email": req.body.email});
+    } else {
         res.status(200).json({"message": "Please use a POST request."});
-    // }
+    }
 }
