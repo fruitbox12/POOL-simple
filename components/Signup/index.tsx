72dcffd3597c82b23626ed41b82810d6ff574d7b
Dylan <dylanwong007@gmail.com>
2021-11-13 19:31:07 -0500

    Update index.tsx

--- components/Signup/index.tsx
+++ components/Signup/index.tsx
@@ -1,33 +1,39 @@
-import { useState, FormEvent } from "react";
+// import { useState, FormEvent } from "react";
 import { useLogin } from "../../context/UserContext";
 
+import { useEffect } from "react";
 import styles from "./Signup.module.scss";
 
 const Signup = (): JSX.Element => {
-    const [email, setEmail] = useState("");
+    // const [email, setEmail] = useState("");
 
     const login = useLogin();
 
-    const handleSubmit = (e: FormEvent) => {
-        e.preventDefault();
-        login(email);
-    };
+    // const handleSubmit = (e: FormEvent) => {
+    //     e.preventDefault();
+    //     login(email);
+    // };
+
+    useEffect(function mount() {
+        console.log("Email was: " + localStorage.getItem("inputEmail"));
+        login(localStorage.getItem("inputEmail") || "");
+    })
 
     return (
         <section className={styles.signup}>
-            <h1>Magic Wallet</h1>
+            {/* <h1>Magic Wallet</h1>
             <img src="images/pooltogether-token--purple-gradient.svg" alt="Magic Wallet" />
-            <span>Log in to your wallet</span>
-            <form onSubmit={handleSubmit}>
+            <span>Log in to your wallet</span> /}
+            {/ <form onSubmit={handleSubmit}>
                 <input
                     type="email"
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
                     placeholder="hello@example.com"
                 />
-
+    
                 <button type="submit">Log in</button>
-            </form>
+            </form> */}
         </section>
     );
 };
